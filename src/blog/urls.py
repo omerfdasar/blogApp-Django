65e9cc9536f68f38d2796e719340c0cde56671b2
Omer <omerfarukdasar@gmail.com>
2022-06-21 20:00:22 +0200

    slugify

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from blog.views import post_list
+from blog.views import post_create, post_list
 
+app_name = "blog"
 urlpatterns = [
     path('', post_list, name='list'),
+    path('create/', post_create, name='create'),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from blog.forms import PostForm
 from .models import Post
 
 
 def post_list(request):
-    qs = Post.objects.all()
+    qs = Post.objects.filter(status ='Published')
     context = {
         'object_list': qs
     }
     return render(request, 'blog/post_list.html', context)
+
+
+def post_create(request):
+    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog:list')
+    context = {
+        'form': form
+    }
+    return render(request, 'blog/post_create.html', context)
